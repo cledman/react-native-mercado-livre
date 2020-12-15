710afc6a4ff41660ae7ae2e8396998e85a013122
cledson <cledman@gmail.com>
2020-12-15 13:35:10 -0300

    Home buttons created.

---
+++ src/components/Button/styles.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    btn_primary: {
+        alignItems: 'center',
+        backgroundColor: '#3483fa',
+        borderRadius: 3,
+        height: 60,
+        justifyContent: 'center',
+        marginTop: 10,
+        width: '100%',
+    },
+    btn_outline_primary: {
+        alignItems: 'center',
+        backgroundColor: '#fff',
+        borderColor: '#3483fa',
+        borderRadius: 3,
+        borderWidth: 2,
+        height: 60,
+        justifyContent: 'center',
+        marginTop: 10,
+        width: '100%',
+    }    
+})
+
+export default styles;
--- src/view/Home/index.js
+++ src/view/Home/index.js
@@ -2,11 +2,17 @@ import React from 'react';
 import { View, Text, Image, TextInput, TouchableOpacity } from 'react-native';
 
 import styles from './styles';
+import Button from '../../components/Button';
+
+import logo from '../../assets/logo.png';
 
 export default function Home() {
     return (
         <View style={styles.container}>
-            <Text>Home</Text>
+            <Image source={logo} />
+            <Button style="outline"><Text>Teste</Text></Button>
+            <Button><Text>Teste</Text></Button>            
         </View >
     );
 }
+
