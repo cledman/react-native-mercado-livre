316d392bcfae0d801df8519e72869a2ac89f8528
cledson <cledman@gmail.com>
2020-12-15 11:35:31 -0300

    First Commit. Firs view created.

---
+++ src/Navigation.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from '@react-navigation/stack';
+import { Text } from 'react-native';
+
+const Stack = createStackNavigator()
+
+import Home from "./view/Home";
+
+export default function Navigation() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Home" component={Home} />
+            </Stack.Navigator>
+
+        </NavigationContainer>
+    )
+}
