a7411b0152515410654e6b2e2d3917e227bb597d
cledson <cledman@gmail.com>
2020-12-16 17:05:59 -0300

    Minor changes on index

--- src/view/Index/index.js
+++ src/view/Index/index.js
@@ -1,15 +1,64 @@
 import React from 'react';
-import { View, Image, Alert } from 'react-native';
+import { View, Text, Image, Alert } from 'react-native';
 import { createDrawerNavigator, DrawerContentScrollView, DrawerItem } from '@react-navigation/drawer'
-//import { AntDesign } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
 
 import styles from './styles';
 import Navigation from './Navigation';
 const Drawer = createDrawerNavigator();
 
+const HeaderDrawer = () =>{
+    return (
+        <View>
+            <View>
+                <Text>Olá</Text>
+            </View>
+            <View>
+               <Text>Mercado Pago</Text> 
+            </View>            
+        </View>
+    )
+}
+
+
+const DrawerContent = ( { navigation }) => {
+    const  listMenuDrawer  = [
+        {
+            id: 1,
+            name: "Home",
+            action: "Main",
+            icon: <AntDesign name="home" size={24} color="black" />,
+        },
+        {
+            id: 2,
+            name: "Procurar",
+            action: "List",
+            icon: <AntDesign name="search1" size={24} color="black" />,
+        },        
+    ]
+
+    return (
+        <View >
+          <HeaderDrawer />
+          <View>
+              <DrawerContentScrollView>
+                  {listMenuDrawer.map((menu) =>(
+                      <DrawerItem 
+                        key={menu.id}
+                        label={menu.name}
+                        icon={()=>menu.icon}
+                        onPress={()=>navigation.navigate(menu.action)}
+                      />
+                  ))}
+              </DrawerContentScrollView>
+          </View>
+        </View>
+    )
+}
+
 export default function Index() {
     return (
-        <Drawer.Navigator>
+        <Drawer.Navigator drawerContent={DrawerContent} >
             <Drawer.Screen 
                 component = {Navigation}
                 name="Navigation"
